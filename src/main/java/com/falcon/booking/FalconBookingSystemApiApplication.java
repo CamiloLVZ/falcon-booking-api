81e0d7a1f783d318a3f89249e98315baa01063bc
Camilo <camilolon6@gmail.com>
2026-01-11 22:57:17 -0500

    merge: link local project with remote repository

---
+++ src/main/java/com/falcon/booking/FalconBookingSystemApiApplication.java
@@ -0,0 +1,13 @@
+package com.falcon.booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FalconBookingSystemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FalconBookingSystemApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/falcon/booking/FalconBookingSystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.falcon.booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FalconBookingSystemApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
