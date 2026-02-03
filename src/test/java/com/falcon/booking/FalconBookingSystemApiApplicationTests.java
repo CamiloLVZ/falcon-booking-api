2d7d98fef3f8c40dbe46550afa98752f7c84531c
Camilo <camilolon6@gmail.com>
2026-02-03 16:34:08 -0500

    feat: set configuration files for deployment

--- src/test/java/com/falcon/booking/FalconBookingSystemApiApplicationTests.java
+++ src/test/java/com/falcon/booking/FalconBookingSystemApiApplicationTests.java
@@ -2,8 +2,10 @@ package com.falcon.booking;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("tests")
 class FalconBookingSystemApiApplicationTests {
 
 	@Test
