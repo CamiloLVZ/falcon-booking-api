375dbd257bb061944bbf49cca15ac62f3edc29dd
Camilo <camilolon6@gmail.com>
2026-02-06 17:36:53 -0500

    feat: add validation for path variable sizes

--- src/main/java/com/falcon/booking/web/controller/AirportController.java
+++ src/main/java/com/falcon/booking/web/controller/AirportController.java
@@ -2,14 +2,17 @@ package com.falcon.booking.web.controller;
 
 import com.falcon.booking.domain.service.AirportService;
 import com.falcon.booking.web.dto.AirportDto;
+import jakarta.validation.constraints.Size;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/airports")
+@Validated
 public class AirportController {
 
     private final AirportService airportService;
@@ -20,7 +23,8 @@ public class AirportController {
     }
 
     @GetMapping("/{iataCode}")
-    public ResponseEntity<AirportDto> getAirport(@PathVariable String iataCode) {
+    public ResponseEntity<AirportDto> getAirport(@PathVariable @Size(min = 3, max = 3, message = "Iata Code must be a String with 3 characters")
+                                                     String iataCode) {
         return ResponseEntity.ok(airportService.getAirportByIataCode(iataCode));
     }
 
--- src/main/java/com/falcon/booking/web/controller/CountryController.java
+++ src/main/java/com/falcon/booking/web/controller/CountryController.java
@@ -4,8 +4,10 @@ import com.falcon.booking.domain.service.AirportService;
 import com.falcon.booking.domain.service.CountryService;
 import com.falcon.booking.web.dto.AirportDto;
 import com.falcon.booking.web.dto.CountryDto;
+import jakarta.validation.constraints.Size;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +17,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/countries")
+@Validated
 public class CountryController {
 
     private final CountryService countryService;
@@ -27,7 +30,8 @@ public class CountryController {
     }
 
     @GetMapping("/{isoCode}")
-    public ResponseEntity<CountryDto> getCountry(@PathVariable String isoCode) {
+    public ResponseEntity<CountryDto> getCountry(@PathVariable @Size(min = 2, max = 2,  message = "Iso Code must be a String with 2 characters")
+                                                     String isoCode) {
         CountryDto country = countryService.getCountryByIsoCode(isoCode);
         return ResponseEntity.ok(country);
     }
@@ -39,7 +43,7 @@ public class CountryController {
     }
 
     @GetMapping("/{isoCode}/airports")
-    public ResponseEntity<List<AirportDto>> getAirportsByCountryIsoCode(@PathVariable String isoCode) {
+    public ResponseEntity<List<AirportDto>> getAirportsByCountryIsoCode(@PathVariable @Size(min = 2, max = 2) String isoCode) {
 
         return ResponseEntity.ok(airportService.getAirportsByCountryIsoCode(isoCode));
     }
