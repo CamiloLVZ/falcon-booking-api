092a3b1087db0d1244a8f6432f39faa03d86d3e5
Camilo <camilolon6@gmail.com>
2026-02-06 19:53:44 -0500

    refactor: remove unused imports

--- src/main/java/com/falcon/booking/persistence/repository/AirplaneTypeRepository.java
+++ src/main/java/com/falcon/booking/persistence/repository/AirplaneTypeRepository.java
@@ -1,13 +1,9 @@
 package com.falcon.booking.persistence.repository;
 
-import com.falcon.booking.domain.valueobject.AirplaneTypeStatus;
 import com.falcon.booking.persistence.entity.AirplaneTypeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface AirplaneTypeRepository extends JpaRepository<AirplaneTypeEntity, Long>, JpaSpecificationExecutor<AirplaneTypeEntity> {
