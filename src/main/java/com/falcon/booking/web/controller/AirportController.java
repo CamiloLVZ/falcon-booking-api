7ef086e1d5e8eded5c8d5c5bf43c765f46ad3b03
Camilo <camilolon6@gmail.com>
2026-02-25 08:48:13 -0500

    feat: add documentation annotations for Airport endpoints

--- src/main/java/com/falcon/booking/web/controller/AirportController.java
+++ src/main/java/com/falcon/booking/web/controller/AirportController.java
@@ -2,6 +2,15 @@ package com.falcon.booking.web.controller;
 
 import com.falcon.booking.domain.service.AirportService;
 import com.falcon.booking.web.dto.AirportDto;
+import com.falcon.booking.web.exception.Error;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.constraints.Size;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,7 +18,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@Tag(name="Airports", description = "Airport retrieval operations")
 @RestController
 @RequestMapping("/v1/airports")
 @Validated
@@ -22,12 +31,29 @@ public class AirportController {
         this.airportService = airportService;
     }
 
+
+    @Operation(summary = "Get a record by its IATA code", description = "Returns an airport record searching by its unique three character IATA code")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Airport record retrieved successfully"
+                    , content = @Content(mediaType = "application/json", schema = @Schema(implementation = AirportDto.class))),
+            @ApiResponse(responseCode = "400", description = "Error by invalid IATA code"
+                    , content = @Content(mediaType = "application/json", schema = @Schema(implementation = Error.class))),
+            @ApiResponse(responseCode = "404", description = "Airport record not found"
+                    , content = @Content(mediaType = "application/json", schema = @Schema(implementation = Error.class))),
+    })
     @GetMapping("/{iataCode}")
     public ResponseEntity<AirportDto> getAirport(@PathVariable @Size(min = 3, max = 3, message = "Iata Code must be a String with 3 characters")
+                                                     @Parameter(description = "Airport unique three character IATA code", example = "BOG")
                                                      String iataCode) {
         return ResponseEntity.ok(airportService.getAirportByIataCode(iataCode));
     }
 
+
+    @Operation(summary = "Get all airports", description = "Returns a list with all registered airports")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Airports list retrieved successfully, even if it is empty.",
+            content = @Content (mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = AirportDto.class)))),
+    })
     @GetMapping
     public ResponseEntity<List<AirportDto>> getAirports() {
         return ResponseEntity.ok(airportService.getAllAirports());
--- src/main/java/com/falcon/booking/web/dto/AirportDto.java
+++ src/main/java/com/falcon/booking/web/dto/AirportDto.java
@@ -1,3 +1,16 @@
 package com.falcon.booking.web.dto;
 
-public record AirportDto(String iataCode, String name, String city, CountryDto country, String timezone) { }
+import io.swagger.v3.oas.annotations.media.Schema;
+
+public record AirportDto(
+        @Schema(description = "Unique three character airport identification by IATA", example = "BOG")
+        String iataCode,
+        @Schema(description = "Common airport name", example = "Aeropuerto Internacional El Dorado")
+        String name,
+        @Schema(description = "City where the airport is located", example = "Bogota")
+        String city,
+        @Schema(description = "Country where the airport is located")
+        CountryDto country,
+        @Schema(description = "Time zone ID where the airport is located", example = "America/Bogota")
+        String timezone)
+{ }
