cd6a7528e9268f9927e61ecda79aad5cf4953d3a
Camilo <camilolon6@gmail.com>
2026-04-03 19:30:36 -0500

    feat: disable data initializer for test environment

--- src/main/java/com/falcon/booking/security/init/DataInitializer.java
+++ src/main/java/com/falcon/booking/security/init/DataInitializer.java
@@ -6,9 +6,11 @@ import com.falcon.booking.web.dto.user.CreateUserDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Component
+@Profile("!tests")
 public class DataInitializer implements CommandLineRunner {
 
     private final UserService userService;
