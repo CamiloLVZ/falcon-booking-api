00c3bd6f7c68e20cc1c0ceb8dea2c8b0cd51324b
Camilo <camilolon6@gmail.com>
2026-07-31 23:14:34 -0500

    refactor: implement equals, hashCode, and toString methods in BoardingPassPdfResult

--- src/main/java/com/falcon/booking/feature/boarding/pdf/BoardingPassPdfResult.java
+++ src/main/java/com/falcon/booking/feature/boarding/pdf/BoardingPassPdfResult.java
@@ -2,8 +2,35 @@ package com.falcon.booking.feature.boarding.pdf;
 
 import com.falcon.booking.feature.boarding.dto.BoardingPassDocumentData;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 public record BoardingPassPdfResult(
         BoardingPassDocumentData document,
         byte[] pdf
 ) {
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BoardingPassPdfResult that = (BoardingPassPdfResult) o;
+        return Objects.equals(document, that.document)
+                && Arrays.equals(pdf, that.pdf);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hashCode(document);
+        result = 31 * result + Arrays.hashCode(pdf);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "BoardingPassPdfResult[" +
+                "document=" + document +
+                ", pdf=" + Arrays.toString(pdf) +
+                ']';
+    }
 }
